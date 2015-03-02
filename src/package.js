a8c3650f07781ae52fd377b73f2e5f91f9bfddd6
xamfoo <xamfoo@all.repo>
2015-03-02 15:04:58 +0800

    Bumped version to 0.0.4

--- src/package.js
+++ src/package.js
@@ -2,7 +2,7 @@ Npm.depends({'esprima': '2.0.0'});
 
 Package.describe({
   name: 'xamfoo:isojs',
-  version: '0.0.2',
+  version: '0.0.4',
   summary: 'Build plugin: Parses and removes extraneous code for the server and client',
   git: 'https://github.com/xamfoo/isojs',
   documentation: 'README.md'
